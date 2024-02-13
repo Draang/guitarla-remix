f03f8d081e56c60768cc388d117863b3675927d4
Angel Contreras Dueñas <c3po.angel@gmail.com>
2024-02-13 16:57:08 -0600

    guitarra individual

--- app/models/guitarras.server.js
+++ app/models/guitarras.server.js
@@ -3,3 +3,10 @@ export async function getGuitarras() {
   const res = await req.json();
   return res;
 }
+export async function getGuitarra(url) {
+  const req = await fetch(
+    `${process.env.API_URL}/guitarras?filters[url]=${url}&populate=imagen`
+  );
+  const res = await req.json();
+  return res.data;
+}
---
+++ app/routes/guitarras.$url.jsx
@@ -0,0 +1,39 @@
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarra } from "../models/guitarras.server";
+import styles from "../styles/guitarras.css";
+
+export async function loader({ params }) {
+  const { url } = params;
+  const guitarra = await getGuitarra(url);
+  return guitarra;
+}
+export function meta({ data }) {
+  return [
+    {
+      title: `GuitarLA - ${data[0].attributes.nombre}`,
+      description: "GuitarLA Nuestra coleccion de guitarras",
+    },
+  ];
+}
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
+export default function Guitarra() {
+  const guitarra = useLoaderData();
+  const { nombre, desc, precio, url, imagen } = guitarra[0].attributes;
+  return (
+    <main className="contenedor guitarra">
+      <img src={imagen.data.attributes.url} alt={nombre} className="imagen" />
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="texto">{desc}</p>
+        <p className="precio">${precio}</p>
+      </div>
+    </main>
+  );
+}
